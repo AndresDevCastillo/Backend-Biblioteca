3e31bc199366bc3e0d8ed930b415abd73ba1b335
Andres Castillo <115095588+AndresDevCastillo@users.noreply.github.com>
2023-07-28 13:22:26 -0500

    estado en procesos

--- src/estado-equipo/dto/update-estado-equipo.dto.ts
+++ src/estado-equipo/dto/update-estado-equipo.dto.ts
@@ -2,6 +2,5 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateEstadoEquipoDto } from './create-estado-equipo.dto';
 
 export class UpdateEstadoEquipoDto extends PartialType(CreateEstadoEquipoDto) {
-
-
+    id: number;
 }
